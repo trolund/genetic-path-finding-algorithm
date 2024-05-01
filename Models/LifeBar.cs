3b4b3abedf36b85e4c4867242ee4faad321ec78a
Troels Lund <trolund@gmail.com>
2024-05-01 08:08:26 +0200

    life bar added

---
+++ Models/LifeBar.cs
@@ -0,0 +1,50 @@
+using System.Drawing;
+using System.Numerics;
+using System.Threading.Tasks;
+using Blazor.Extensions.Canvas.Canvas2D;
+
+namespace BlazorCanvasTest2.Models
+{
+    public class LifeBar {
+
+        private int Max;
+        private int Progress;
+        private Color outer;
+        private Color inner;
+
+        private Vector2 Pos;
+
+
+        public LifeBar(Vector2 pos, int progress, int max) {
+            Max = max;
+            Progress = progress;
+            Pos = pos;
+            outer = Color.FromArgb(0, 0, 250);
+            inner = Color.FromArgb(155, 60, 0);
+        }
+
+        public void Reset(){
+            Progress = 0;
+        }
+        
+        public void StepForward(int progress)
+        {
+            Progress = progress;
+        }
+        public async Task Display(Canvas2DContext ctx)
+        {
+            var p = Max/100 * Progress * 2;
+
+            await ctx.BeginPathAsync();
+            await ctx.SetFillStyleAsync("#8b95a3");
+            await ctx.FillRectAsync(Pos.X, Pos.Y, 280, 30);
+            await ctx.FillAsync();
+
+            await ctx.BeginPathAsync();
+            await ctx.SetFillStyleAsync("#1f824c");
+            await ctx.FillRectAsync(Pos.X + 5, Pos.Y + 5, p, 20);
+            await ctx.FillAsync();
+        }
+    }
+}
+
